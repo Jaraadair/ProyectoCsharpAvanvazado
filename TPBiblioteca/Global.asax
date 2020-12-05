f7bf8f32454437be37254d10ee1e38d287feb52b
Jaraadair <54456958+Jaraadair@users.noreply.github.com>
2020-12-05 00:19:57 -0300

    Agregar archivos de proyecto.

---
+++ TPBiblioteca/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TPBiblioteca.MvcApplication" Language="C#" %>
---
+++ TPBiblioteca/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TPBiblioteca.Startup))]
+namespace TPBiblioteca
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
