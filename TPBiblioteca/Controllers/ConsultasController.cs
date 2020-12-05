63e32351abba53b018257a2ce524cb81fc2a7fd5
Jaraadair <54456958+Jaraadair@users.noreply.github.com>
2020-12-05 11:36:24 -0300

    - Proyecto ASP MVC con Web Api con autenticación - Página de Inicio estática presentando las bondades del sistema. Es la landing page de su sistema. Utilizar componentes y creatividad para crearlo. Pueden utilizar plantillas html - Página de Nosotros. Una lista de los integrantes con foto y mini bio. Ser creativos - Página de Contacto. Crear un formulario

---
+++ TPBiblioteca/Controllers/ConsultasController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPBiblioteca.Models;
+
+namespace TPBiblioteca.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private BibliotecaEntities db = new BibliotecaEntities();
+
+        // GET: Consultas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
